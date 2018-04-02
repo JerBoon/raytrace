4c7206f34509a402356237fbabb2484713b7b9a4
Jer Boon <jer.booner@gmail.com>
2018-04-02 12:01:49 +0100

    documentation of RT.Surface() function

--- R/surface.R
+++ R/surface.R
@@ -4,13 +4,21 @@
 #---------------------------------------------------------------------
 #' Validate parameters, and return a valid surface object
 #'
+#' @param rgb Either a 3 value vector of numbers in teh range 0 to 1, or a function which will return the equivalent.
+#'     The function, if present, will be passed the values of *north* and *east* as returned by the object's intersection.
+#'     The function allow interesting surface properties to be easily constructed, for example patterned, image mapping, smoothing or 
+#'     randomisation.
+#' @param transparency The proportion of incident light which will be refracted by the surface. 1 minus this value will be reflected.
+#'     Default = 0, i.e. a non-transparent surface.
+#' @param matteness The proportion of matte reflectivity of the surface. 1 minus this value will be reflected as mirror reflection.
+#'     Default = 0, i.e the surface has mirror reflectivity
 #'
-#' @return Surface object
+#' @return A validated surface object. Ray tracing functions in this package expect all solid objects to have a valid surface property.
 #' 
 #' @export
 #'
 #' @examples
-#'   surf <- RT.asSurface(rgb=c(1,1,0)))
+#'   surf <- RT.Surface(rgb=c(1,1,0), transparency=1)
 
 # ultimately I think I need...
 #  - colour
